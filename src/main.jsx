3d465374775ac05c398b127df9a77da7f9b6f8f3
Theo <ojw1912@gmail.com>
2025-02-25 09:52:51 +0900

    250225 09:50 # 41. 로그아웃 액션 처리

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+import { Form } from "react-router-dom";
+
+const Main = ({ userData }) => {
+
+  const { role, email } = userData;
+
+  return (
+    <>
+      <h2>{email}님 환영합니다.</h2>
+      <h3>현재 권한: [ {role} ]</h3>
+
+      <Form
+        action='/logout'
+        method='POST'>
+        <button>Logout</button>
+      </Form>
+    </>
+  );
+};
+
+export default Main;
