bdecf0f43899607cc632ef974b5207f6ceab6313
Theo <ojw1912@gmail.com>
2025-02-26 11:35:05 +0900

    250226 11:20 # 46. 컨텍스트로 총 이벤트 생성 개수 전역관리

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import EventList from "../components/EventList";
 import EventSkeleton from "../components/EventSkeleton";
 import { EVENT_API_URL } from "../config/host-config";
 import { fetchWithAuth } from "../services/api";
+import EventContext from "../context/event-context";
 
 const EventsPage = () => {
+
+  // 컨텍스트에서 총 이벤트 수를 갱신하는 함수를 소비
+  const { changeTotalEventCount } = useContext(EventContext);
 
   // 무한스크롤 옵저버가 감시할 태그 ref
   const observerRef = useRef();
@@ -25,7 +29,7 @@ const EventsPage = () => {
   const [loading, setLoading] = useState(false);
 
   // 서버에서 데이터를 불러오는 함수
-  const fetchEvents = async () => { 
+  const fetchEvents = async () => {
 
     if (isFinish || loading) return;
 
@@ -35,6 +39,9 @@ const EventsPage = () => {
 
     const response = await fetchWithAuth(`${EVENT_API_URL}?sort=id&page=${currentPage}`);
     const { hasNext, eventList: events, totalCount } = await response.json();
+
+    // 총 이벤트 수를 컨텍스트에 저장
+    changeTotalEventCount(totalCount);
 
     setEventList(prev => [...prev, ...events]);
     setIsFinish(!hasNext);
